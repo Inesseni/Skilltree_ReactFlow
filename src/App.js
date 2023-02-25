3255bc7bb3b3d655f622297a6f5a5e4418b5bce2
Inesseni <ineshilz@yahoo.de>
2023-02-25 12:51:49 +0100

    Save my work before Ines delets it

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import ReactFlow from "reactflow";
-import React, {useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import "reactflow/dist/style.css";
 import "./App.css";
 
 import MyNodes from './library/MyNodes';
 import MyEdges from './library/MyEdges';
-import {MyStyledDiv, Header, TreeWrapper, MyH1, MyH2, DescriptionWrapper, Image, ImageWrapper} from './library/MyStyledComponents';
+import { MyStyledDiv, Header, TreeWrapper, MyH1, MyH2, DescriptionWrapper, Image, ImageWrapper } from './library/MyStyledComponents';
 import Description from "./components/Description";
 
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 var fontSizeH1 = clamp((window.innerWidth / 10), 80, 600);
 
 function App() {
-
-  const [myState, setMyState] = useState({
-    selectedNode: 0,
-    description: '',
-    title: '',
-    nodesCopy: MyNodes,
-    focusedNode: null,
-    nodeClicked: false,
-  });
+  const [selected, setSelected] = useState(undefined)
+  const [hoveredNode, setHoveredNode] = useState(undefined)
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
-  const [opacity, setOpacity] = useState(0);
-  const [image, setImage] = useState(myState.nodesCopy[1].imgLink);
 
   useEffect(() => {
     const handleMouseMove = (event) => {
@@ -39,97 +30,50 @@ function App() {
     };
   }, []);
 
-
-
   const entered = (event, node) => {
-    var newNode = {node}.node.id;
-    setMyState({ ...myState, focusedNode : newNode})
-
-
-    
-    if(myState.nodeClicked) return;
-    var newImg = myState.nodesCopy[newNode-1].imgLink;
-    setImage(newImg);
-    if(image != null){
-      setOpacity(1);
-}
-    updateDescription(newNode);
+    if (selected !== undefined) return;
+    setHoveredNode(node.id)
   };
 
 
   const exit = (event, node) => {
-    if(myState.nodeClicked) return;
-    setMyState({ ...myState, title : "", description : ""})
-
-    setOpacity(0);
+    setHoveredNode(undefined)
   };
 
-  
-  const selected = (event, node) => {
-    var newNode = {node}.node.id;
-
-    ////Same node clicked
-    if(newNode === myState.selectedNode){
-
-      //copy nodes array and de-select all nodes
-      let updatedNodes = [...myState.nodesCopy];
-      updatedNodes.forEach((n) => {
-        n.selected = false;
-      });
-
-      //update the selected node, if a node is clicked and the new (unselected) node Array
-      setMyState({ ...myState, selectedNode : null, nodesCopy : updatedNodes, nodeClicked : false})
-
-    ///// New node clicked
-    }else{
-      //console.log("new node");
-
-      let updatedNodes = [...myState.nodesCopy];
-      updatedNodes.forEach((n) => {
-      n.selected = n.id === node.id;
-    });
-    
-      var nodee = myState.nodesCopy[newNode - 1];
-      setMyState({ ...myState, description: nodee.text, title : nodee.title, selectedNode : nodee.id, nodesCopy : updatedNodes, nodeClicked : true})
-    }
-  };
-
-
-  const updateDescription = (nodeId) => {
-    var node = myState.nodesCopy[nodeId -1];
-    setMyState({ ...myState, description: node.text, title: node.title});
-  };
-
-
-
+  const selectedOrHoveredNode = MyNodes.find((n) => n.id === (selected ?? hoveredNode ?? -1))
   return (
     <MyStyledDiv>
       <Image style={{
         top: cursorPosition.y - 250,
         left: cursorPosition.x - 500,
-      }} 
-      src={image} 
-      alt="" 
-      opacity={opacity}/>
+      }}
+        src={selectedOrHoveredNode !== undefined
+          ? selectedOrHoveredNode.imgLink
+          : undefined}
+        alt=""
+        opacity={selectedOrHoveredNode !== undefined ? 1 : 0} />
       <Header>
         <MyH1 width={fontSizeH1}>SKILLTREE</MyH1>
         <MyH2>Ines Hilz</MyH2>
       </Header>
 
       <DescriptionWrapper>
-        <Description description={myState.description} title={myState.title} />
+        {selectedOrHoveredNode !== undefined
+          ? <Description description={selectedOrHoveredNode.text} title={selectedOrHoveredNode.title} />
+          : null
+        }
       </DescriptionWrapper>
 
       <TreeWrapper>
         <ReactFlow
-          nodes={myState.nodesCopy}
+          nodes={MyNodes.map((n) => n.id === selected ? { ...n, selected: true } : n)}
           edges={MyEdges}
           panOnDrag={false}
           panOnScroll={false}
           preventScrolling={false}
           onNodeMouseEnter={entered}
           onNodeMouseLeave={exit}
-          onNodeClick={selected}
+          onNodeClick={(_, node) => setSelected(node.id === selected ? undefined : node.id)}
         />
       </TreeWrapper>
     </MyStyledDiv>
